c09c6943cc58dbb7dd66f88ceb00ae3444772b19
matteoaricci <matteo.a.ricci@gmail.com>
2020-07-13 13:34:56 -0400

    flip astro with space

--- Astronaut.lua
+++ Astronaut.lua
@@ -1,11 +1,13 @@
 Astronaut = Class{}
 
-local gravity = 50
+local gravity = 150
 
 function Astronaut:init()
     self.image = love.graphics.newImage('/images/astro-sprite-blue.png')
     self.width = self.image:getWidth()
     self.height = self.image:getHeight()
+    self.flip = 1
+    self.rotation = 0
 
     self.x = VIRTUAL_WIDTH / 10
     self.y = VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 4
@@ -14,8 +16,13 @@ end
 
 function Astronaut:update( dt )
     self.y = self.y + gravity * dt 
+
+    if love.keyboard.wasPressed('space') then 
+        gravity = -gravity
+        self.flip = -self.flip
+    end
 end
 
 function Astronaut:render()
-    love.graphics.draw(self.image, self.x, self.y)
+    love.graphics.draw(self.image, self.x, self.y, self.rotation, 1, self.flip, 0, self.height/2)
 end
--- main.lua
+++ main.lua
@@ -35,6 +35,8 @@ function love.load()
         fullscreen = false,
         resizable = true
     })
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -42,8 +44,18 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+
     if key == 'escape' then 
         love.event.quit()
+    end
+end
+
+function love.keyboard.wasPressed( key )
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
     end
 end
 
@@ -55,6 +67,8 @@ function love.update(dt)
     % (LOOP_POINT / 3)
 
     astro:update(dt)
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
